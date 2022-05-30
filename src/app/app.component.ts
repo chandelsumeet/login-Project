48e30286639b53aab8057abf18e2d996dbbb2ce6
sumeet chandel <sumeetchandel321@gmail.com>
2022-05-30 13:54:56 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MsalService } from '@azure/msal-angular';
+import { AuthenticationResult } from '@azure/msal-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'login-project';
+  constructor(private msalService : MsalService)
+  {
+    
+  }
+  ngOnInit(): void {
+    this.msalService.instance.handleRedirectPromise().then((res)=>{
+if(res!=null && res.account!=null)
+{
+  this.msalService.instance.setActiveAccount(res.account);
+}
+    })
+    throw new Error('Method not implemented.');
+  }
+  isLoggedIn()
+  {
+    console.log(this.msalService.instance.getActiveAccount() !=null)
+    return this.msalService.instance.getActiveAccount() !=null;
+  }
+
+  login()
+  {
+    this.msalService.loginRedirect();
+    // this.msalService.loginPopup().subscribe((res:AuthenticationResult)=>{
+    //  
+    // });
+  }
+
+  logout()
+  {
+    this.msalService.logout();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MsalModule, MsalService, MSAL_INSTANCE } from '@azure/msal-angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PublicClientApplication, InteractionType, BrowserCacheLocation, IPublicClientApplication } from "@azure/msal-browser";
+export function MSALInstanceFactory():IPublicClientApplication
+{
+  return new PublicClientApplication(
+    {
+      auth:{
+        clientId:'f3798c7e-82a5-4de5-a1d3-11370cf8c11c',
+        redirectUri:'http://localhost:4200',
+      }
+    }
+  )
+}
+
 
 @NgModule({
   declarations: [
@@ -10,9 +24,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MsalModule
   ],
-  providers: [],
+  providers: [{
+    provide:MSAL_INSTANCE,
+    useFactory:MSALInstanceFactory
+  },
+  MsalService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
